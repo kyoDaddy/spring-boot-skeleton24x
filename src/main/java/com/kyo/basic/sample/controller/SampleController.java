4f25a80e800eb9a5032910d26a68ff7d01457245
rlarbghrbgh <rlarbghrbgh@gmail.com>
2022-01-19 14:27:41 +0900

    스프링이 제공하는 controller 주로 사용하는 input sample.

--- src/main/java/com/kyo/basic/sample/controller/SampleController.java
+++ src/main/java/com/kyo/basic/sample/controller/SampleController.java
@@ -6,13 +6,15 @@ import com.kyo.basic.base.utils.PrintUtils;
 import com.kyo.basic.sample.service.SampleService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpMethod;
+import org.springframework.util.MultiValueMap;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
+import java.util.Locale;
 
 @Slf4j
 @RestController
@@ -40,5 +42,25 @@ public class SampleController {
         return CustomResponseEntity.success(sampleService.getSamples());
     }
 
+
+    @RequestMapping("/headers")
+    public String headers(HttpServletRequest request,
+                          HttpServletResponse response,
+                          HttpMethod httpMethod,
+                          Locale locale,
+                          @RequestHeader MultiValueMap<String, String> headerMap, //하나의 키에 여러값을 받을 수 있다.
+                          @RequestHeader("host") String host,
+                          @CookieValue(value = "myCookie", required = false) String cookie
+                          ){
+
+        log.info("request={}", request);
+        log.info("response={}", response);
+        log.info("httpMethod={}", httpMethod);
+        log.info("locale={}", locale);
+        log.info("headerMap={}", headerMap);
+        log.info("header host={}", host);
+        log.info("myCookie={}", cookie);
+        return "ok";
+    }
 
 }
